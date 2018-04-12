c75fd0537594fe9232549850bb05cca0c8c62f99
arnold <arnold@ubuntu>
2018-04-12 21:47:34 +0200

    Version 2018.04, first commit.

--- cloudedbats_wurb/wurb_core/pettersson_m500_batmic.py
+++ cloudedbats_wurb/wurb_core/pettersson_m500_batmic.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
 # Project: http://cloudedbats.org
-# Copyright (c) 2016 Arnold Andreasson 
+# Copyright (c) 2016-2018 Arnold Andreasson 
 # License: MIT License (see LICENSE.txt or http://opensource.org/licenses/mit).
 from __future__ import unicode_literals
 
--- cloudedbats_wurb/wurb_raspberry_pi/__init__.py
+++ cloudedbats_wurb/wurb_raspberry_pi/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
 # Project: http://cloudedbats.org
-# Copyright (c) 2016-2017 Arnold Andreasson 
+# Copyright (c) 2016-2018 Arnold Andreasson 
 # License: MIT License (see LICENSE.txt or http://opensource.org/licenses/mit).
 
 from wurb_raspberry_pi.control_by_gpio import ControlByGpio
